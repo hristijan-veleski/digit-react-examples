6c0e24a8cefde64b1824b75cd58a1ca044207f9d
Hristijan Veleski <hristijan1veleski@gmail.com>
2023-12-10 01:40:38 +0100

    restructure

--- src/main.jsx
+++ src/main.jsx
@@ -4,57 +4,57 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/Root.jsx";
 import ErrorPage from "./routes/ErrorPage.jsx";
-import Greetings from "./routes/Greetings.jsx";
-import CountAndGreetings from "./routes/CountAndGreetings.jsx";
-import VelocityExample from "./routes/VelocityAnimation.jsx";
-import Movies from "./routes/Movies.jsx";
-import CountReducer from "./routes/CountReducer.jsx";
-import CountContext from "./routes/CountContext.jsx";
-import BlinkyRender from "./routes/BlinkyRender.jsx";
-import PrimeCalculator from "./routes/PrimeCalculator.jsx";
+import Greetings from "./examples/Greetings.jsx";
+import CountAndGreetings from "./examples/CountAndGreetings.jsx";
+import VelocityExample from "./examples/VelocityAnimation.jsx";
+import Movies from "./examples/Movies.jsx";
+import CountReducer from "./examples/CountReducer.jsx";
+import CountContext from "./examples/CountContext.jsx";
+import BlinkyRender from "./examples/BlinkyRender.jsx";
+import PrimeCalculator from "./examples/PrimeCalculator.jsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/greetings",
-    element: <Greetings />,
-  },
-  {
-    path: "/count-and-greetings",
-    element: <CountAndGreetings />,
-  },
-  {
-    path: "/velocity-example",
-    element: <VelocityExample />,
-  },
-  {
-    path: "/movies",
-    element: <Movies />,
-  },
-  {
-    path: "/count-reducer",
-    element: <CountReducer />,
-  },
-  {
-    path: "/count-context",
-    element: <CountContext />,
-  },
-  {
-    path: "/blinky-render",
-    element: <BlinkyRender />,
-  },
-  {
-    path: "/prime-calculator",
-    element: <PrimeCalculator />,
-  },
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/greetings",
+        element: <Greetings />,
+    },
+    {
+        path: "/count-and-greetings",
+        element: <CountAndGreetings />,
+    },
+    {
+        path: "/velocity-example",
+        element: <VelocityExample />,
+    },
+    {
+        path: "/movies",
+        element: <Movies />,
+    },
+    {
+        path: "/count-reducer",
+        element: <CountReducer />,
+    },
+    {
+        path: "/count-context",
+        element: <CountContext />,
+    },
+    {
+        path: "/blinky-render",
+        element: <BlinkyRender />,
+    },
+    {
+        path: "/prime-calculator",
+        element: <PrimeCalculator />,
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 );
